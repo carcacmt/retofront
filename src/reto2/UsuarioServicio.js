50e40a40e8e6b0f8649232cf2f0e49ba3b4800c7
Carlos Jose Caceres Ochoa <91512165+carcacmt@users.noreply.github.com>
2023-07-23 15:35:39 -0500

    Update UsuarioServicio.js

--- src/reto2/UsuarioServicio.js
+++ src/reto2/UsuarioServicio.js
@@ -5,7 +5,7 @@
 import Axios from 'axios'
 //endpoint en servidor de pruebas
 const port = "8090"
-const endpoint =`http://host.docker.internal:${port}/api/user`
+const endpoint =`http://129.153.206.164:${port}/api/user`
 /**funcion para consumir todos los usuarios */
 const getApiUsuarios=()=>{
     return Axios.get(endpoint+"/all")
